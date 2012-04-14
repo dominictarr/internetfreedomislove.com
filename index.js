f23c9f89ec1fad00ee277f56750ab27164b7b522
Dominic Tarr <dominic.tarr@gmail.com>
2012-04-14 18:54:49 +1200

    looks good

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 #! /usr/bin/env node
-
-var connect = require('connect')
+var express = require('express')
+var app = express.createServer()
 var join = require('path').join
+var fs = require('fs')
 
 console.log(join(__dirname, 'public'))
 
-connect()
-  .use(connect.static('public'))
-  .use(connect.bodyParser())
+/*
+  parse a form...
+  set, name, question, placeholder
+    
+*/
+
+var logStream = fs.createWriteStream('signup.log', {flags: 'a', encoding: 'utf-8'})
+
+
+app
+  .use(express.static('public'))
+  .use(express.bodyParser())
+  .use(express.router(function (app) {
+
+    app.post('/signup', function (req, res) {
+      var line = [new Date()]
+      for(var k in req.body)
+        line.push(req.body[k])
+      line = line.map(JSON.stringify).join(',') + '\n'
+      logStream.write(line)
+      console.log(line)
+      res.end('thank you for signing up')
+    })
+
+    app.get('/download', function(req, res) {
+      fs.createReadStream('signup.log').pipe(res)
+    })
+  }))
   .listen(process.env.PORT || 2000)
