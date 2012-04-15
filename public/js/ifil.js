fd73150d61a43c21e3dcd2d664b4b9f155d85f6f
Dominic Tarr <dominic.tarr@gmail.com>
2012-04-15 20:39:05 +1200

    show fields incrementally

---
+++ public/js/ifil.js
@@ -0,0 +1,45 @@
+
+
+$(function () {
+
+  $('input, textarea, select').each(function () {this.required = true})
+
+  var inputs = $('.field')
+  var allowSubmit = false
+  var next
+  inputs.change(function (e) {
+    //show the next item.
+    if(this.nextElementSibling.type === 'submit')
+      return allowSubmit = true
+    if(next === this)
+      next = this.nextElementSibling
+    $(this.nextElementSibling)
+      .show('slow').find('input, textarea, select').first().focus()
+  })
+
+  function pop () {
+    return [].pop.call(inputs)
+  }
+
+  function shift () {
+    return [].shift.call(inputs)
+  }
+
+  var submit = pop()
+
+  shift()
+  next = shift()
+var filtered =
+  inputs/*.filter(function() {
+    var l = $(this)
+      .find('input, textarea, select')
+      .filter(function () {
+        return !this.value
+      })
+    return l.length
+  })*/.hide()
+
+  console.log(filtered)
+
+})
+
